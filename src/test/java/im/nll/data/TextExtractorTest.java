5054514c84c3724c465941164342936ceffaff16
fivesmallq <fivesmallq@gmail.com>
2016-03-22 15:12:12 +0800

    add test

---
+++ src/test/java/im/nll/data/TextExtractorTest.java
@@ -0,0 +1,20 @@
+package im.nll.data;
+
+import org.apache.http.client.fluent.Request;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 16/3/22 下午3:03
+ */
+public class TextExtractorTest {
+
+    @Test
+    public void testExtract() throws Exception {
+        String url = "http://news.cnblogs.com/n/541621/";
+        String html = Request.Get(url).execute().returnContent().asString();
+        String text = TextExtractor.extract(html);
+        System.out.println(text);
+    }
+}
